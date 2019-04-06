572cccb421605de60878e8a8d5bee6fabdecdf53
tresende <thiago.gcresende@gmail.com>
2019-04-06 12:15:05 -0300

    configuração inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Firestore.instance.collection('teste').document('teste-2').setData({
+    'name': 'thiago2',
+  });
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
