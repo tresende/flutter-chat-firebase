35eb67dd85d6c5d3c40a6dbd66ac15afe17968c0
tresende <thiago.gcresende@gmail.com>
2019-04-06 12:28:39 -0300

    criando nova collection

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  Firestore.instance.collection('mensagens').document().setData({
+  Firestore
+  .instance
+  .collection('mensagens')
+  .document()
+  .collection('media')
+  .document()
+  .setData({
     'from': 'Camila',
-    'texto': 'Beleza?',
+    'texto': 'imagem aqui',
   });
   runApp(MyApp());
 }
