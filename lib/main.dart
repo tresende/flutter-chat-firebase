d6308fc925f48b10c9357dc0bd03ee5bc95dcf9a
tresende <thiago.gcresende@gmail.com>
2019-04-07 16:49:57 -0300

    lendo dados do firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  Firestore
-  .instance
-  .collection('mensagens')
-  .document()
-  .collection('media')
-  .document()
-  .setData({
-    'from': 'Camila',
-    'texto': 'imagem aqui',
+void main() async {
+  DocumentSnapshot snapshot = await Firestore.instance
+      .collection('mensagens')
+      .document('-LbnF9Djo0G1OVhvU9iZ')
+      .get();
+  // print(snapshot.documentID);
+  // print(snapshot.data);
+
+  // QuerySnapshot list =
+  //     await Firestore.instance.collection('mensagens').getDocuments();
+  // print(list.documents);
+  // for (DocumentSnapshot doc in list.documents) {
+  //   print(doc.data);
+  // }
+
+  Firestore.instance.collection('mensagens').snapshots().listen((snapshot) {
+    for (DocumentSnapshot doc in snapshot.documents) {
+      print(doc.data);
+    }
   });
+
+  // Firestore
+  // .instance
+  // .collection('mensagens')
+  // .document()
+  // .collection('media')
+  // .document()
+  // .setData({
+  //   'from': 'Camila',
+  //   'texto': 'imagem aqui',
+  // });
   runApp(MyApp());
 }
 
